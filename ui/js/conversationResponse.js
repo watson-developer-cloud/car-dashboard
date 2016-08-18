d1904e60960fefb9fddcf4fe351501bf1e06cc86
MELISSA A. LORD <Melissa.Lord@ibm.com>
2016-08-18 16:31:39 -0400

    ran lint

--- ui/js/conversationResponse.js
+++ ui/js/conversationResponse.js
@@ -81,15 +81,15 @@ var ConversationResponse = (function() {
         cuisine: function() { Panel.mapFoodNumbers(); },
         func: function() { Panel.mapGeneral(); }
       },
-      off_topic: {
-        amenity: {
-          gas: function() { Panel.mapGas(); },
-          restaurant: function() { Panel.mapFoodCuisine(); },
-          restroom: function() { Panel.mapRestrooms(); }
-        },
-        cuisine: function() { Panel.mapFoodNumbers(); },
-        genre: function(value) { Panel.playMusic(value); }
-      }
+      off_topic: {
+        amenity: {
+          gas: function() { Panel.mapGas(); },
+          restaurant: function() { Panel.mapFoodCuisine(); },
+          restroom: function() { Panel.mapRestrooms(); }
+        },
+        cuisine: function() { Panel.mapFoodNumbers(); },
+        genre: function(value) { Panel.playMusic(value); }
+      }
     };
   }
 
