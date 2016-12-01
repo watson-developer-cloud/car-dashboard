cc56f3ed9df9145e31ff3696075ba816c7f68cc6
Steve Green <stevenpkg@au1.ibm.com>
2016-12-01 12:59:44 +1000

    Made some small fixes

--- test/test.express.js
+++ test/test.express.js
@@ -20,8 +20,6 @@ var app = require('../app');
 var bodyParser = require('body-parser');
 var request = require('supertest');
 
-app.use(bodyParser.json());
-
 describe('Basic API tests', function () {
   it('GET to / should load the home page', function (done) {
     request(app).get('/').expect(200, done);
